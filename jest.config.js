ebfb3edcb494842a6b6c63524fc3ee0db994ebbb
joaopedroaquino <aqerhack@gmail.com>
2022-12-09 15:31:44 -0300

    chore: add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  roots: ['rootDir>/src'],
+  roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*ts'],
   coverageDirectory: "coverage",
   testEnvironment: 'node',
