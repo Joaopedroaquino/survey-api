d91b1bac3c9da1906d7d4ffda28c09392057d1ce
joaopedroaquino <aqerhack@gmail.com>
2022-12-10 11:34:37 -0300

    refactor: move server error to a http-helper

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,49 +1,42 @@
 import { InvalidParamError } from "../errors/invalid-param-error"
 import { MissingParamError } from "../errors/missing-param-error"
-import { ServerError } from "../errors/server-error"
-import { badRequest } from "../helpers/http-helper"
+import { badRequest, serverError } from "../helpers/http-helper"
 import { Controller } from "../protocols/controller"
 import { EmailValidator } from "../protocols/email-validator"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignUpController implements Controller {
-   private readonly emailValidator: EmailValidator
-    constructor(emailValidator: EmailValidator){
+    private readonly emailValidator: EmailValidator
+    constructor(emailValidator: EmailValidator) {
         this.emailValidator = emailValidator
 
     }
 
 
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = [ 'name', 'email', 'password', 'passwordConfirmation']
+        const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
-        try{
+        try {
 
-        
-        for (const field of requiredFields) {
-            if(!httpRequest.body[field]) {
-                return badRequest(new MissingParamError(field))
+
+            for (const field of requiredFields) {
+                if (!httpRequest.body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
             }
+
+            const isValid = this.emailValidator.isValid(httpRequest.body.email)
+            if (!isValid) {
+                return badRequest(new InvalidParamError('email'))
+
+            }
+
         }
-
-      const isValid =  this.emailValidator.isValid(httpRequest.body.email)
-        if(!isValid){
-            return badRequest(new InvalidParamError('email'))
-
-        }
-        
-    }
-    catch(error){
-        return {
-            statusCode: 500,
-            body: new ServerError()
-
+        catch (error) {
+            return serverError()
         }
     }
-}
 
-   
-    
 
 }
 
