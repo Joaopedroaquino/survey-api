94f742c714cc506c6596374f2a629d4615a581e1
joaopedroaquino <aqerhack@gmail.com>
2022-12-11 11:17:25 -0300

    refactor: move Ok httpResponde to a helper method

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,5 +1,5 @@
 import { MissingParamError, InvalidParamError } from "../../errors"
-import { badRequest, serverError } from "../../helpers/http-helper"
+import { badRequest, serverError, ok } from "../../helpers/http-helper"
 import { EmailValidator, HttpRequest, HttpResponse, Controller, AddAccount } from "./signup-protocols"
 
 export class SignUpController implements Controller {
@@ -42,10 +42,8 @@ export class SignUpController implements Controller {
                 password
             })
 
-            return {
-                statusCode: 200,
-                body: account
-            }
+            return ok(account)
+            
 
         }
         catch (error) {
