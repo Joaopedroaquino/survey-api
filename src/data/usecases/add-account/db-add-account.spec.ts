efbc1a24155a03917892e666c2801af2148ff30e
joaopedroaquino <aqerhack@gmail.com>
2022-12-11 17:56:47 -0300

    refactor: move sut creator to a factory method

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,15 +1,28 @@
+import { Encrypter } from "../../protocols/encrypter"
 import { DbAddAccount } from "./db-add-account"
 
 describe('DbAddAccount Usecase', ()=>{
-    test('Should call Encrypter with correct password', async ()=> {
-        class EncrypterStub {
-          async  encrypt(value: string): Promise<string> {
-            return new Promise(resolve => resolve('hashed_password'))
 
-            }
-        }
-        const encrypterStub = new EncrypterStub()
-        const sut = new DbAddAccount(encrypterStub)
+    interface SuTypes {
+        sut: DbAddAccount,
+        encrypterStub: Encrypter
+    }
+
+    const makeSut = (): SuTypes => {
+        class EncrypterStub {
+            async  encrypt(value: string): Promise<string> {
+              return new Promise(resolve => resolve('hashed_password'))
+  
+              }
+          }
+          const encrypterStub = new EncrypterStub()
+          const sut = new DbAddAccount(encrypterStub)
+
+          return {sut, encrypterStub}
+
+    }
+    test('Should call Encrypter with correct password', async ()=> {
+       const {sut, encrypterStub} = makeSut()
         const encryptSpy = jest.spyOn(encrypterStub,'encrypt')
         const accountData = {
             name: 'valid_name',
